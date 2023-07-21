085a732c88e45885a0a3d6b388faeffc0da6ba2a
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-07-21 08:48:07 +0000

    Avance/Final

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,8 @@ import "../style/index.css";
  * 
     {
         includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        background: "https://quotesbook.com/quotes/dreams-quotes/unknown-authors/make-your-dreams-come-true-3840x2160-5474.jpg", // this is the url of the image that will used as background for the profile cover
+        avatarURL: "https://scontent.fsyq3-1.fna.fbcdn.net/v/t39.30808-6/361099608_2139293242935592_4739953013994989421_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=6SkR6EbfD50AX_Gyfh6&_nc_ht=scontent.fsyq3-1.fna&oh=00_AfCeieo5oT-ChXKaUG6pRgVPB5iqCEY6s17RWZchy_bP2A&oe=64BFDCDE", // this is the url for the profile avatar
         socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -33,15 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+          <h1>${variables["name"] == null ? "" : variables["name"]} ${
+    variables["lastname"] == null ? "" : variables["lastname"]
+  }</h1>                    
+                  <h2>${variables["role"] == null ? "" : variables["role"]}</h2>
+                  <h3>${variables["city"] == null ? "" : variables["city"]} ,
+                  ${
+                    variables["country"] == null ? "" : variables["country"]
+                  }</h3>
+                  <ul class="${
+                    variables["socialMediaPosition"] == null
+                      ? ""
+                      : variables["socialMediaPosition"]
+                  } ">
+                    <li><a href="https://twitter.com/ ${
+                      variables["twitter"] == null ? "" : variables["twitter"]
+                    } "><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="https://github.com/ ${
+                      variables["github"] == null ? "" : variables["github"]
+                    }"><i class="fab fa-github"></i></a></li>
+                    <li><a href="https://www.linkedin.com/ ${
+                      variables["linkedin"] == null ? "" : variables["linkedin"]
+                    }"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="https://www.instagram.com/ ${
+                      variables["instagram"] == null
+                        ? ""
+                        : variables["instagram"]
+                    }"><i class="fab fa-instagram"></i></a></li>
+                  </ul>
         </div>
     `;
 }
@@ -54,14 +73,15 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://quotesbook.com/quotes/dreams-quotes/unknown-authors/make-your-dreams-come-true-3840x2160-5474.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://avatars.githubusercontent.com/u/128443618?v=4",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "luciasaxjix2",
     linkedin: null,
     instagram: null,
     name: null,
